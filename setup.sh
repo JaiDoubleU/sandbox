66f14b10dfa4516adac181806f9bff1e0606de71
JaiDoubleU <jwshannon@gmail.com>
2025-07-28 09:46:46 -0600

    Add .gitignore to ignore node_modules

--- setup.sh
+++ setup.sh
@@ -2,9 +2,10 @@
 set -e
 
 # Install dependencies (requires network access)
-apt-get update
-apt-get install -y nodejs npm
-npm install react react-dom antd@4.24.16
+#apt-get update
+#apt-get install -y nodejs npm
+# npm install react react-dom antd@4.24.16
+npm install react@18.3.1 react-dom@18.3.1 antd@4.24.16
 
 # Set up Storybook
 npx sb init
